bc5358e0a0e36e2ccd833a3f2942abbadf45b606
altinkuko <altinkuko@gmail.com>
2023-09-18 18:49:20 +0200

    Create admin on init

---
+++ src/main/java/com/example/medical_center/config/MedicalConfig.java
@@ -0,0 +1,28 @@
+package com.example.medical_center.config;
+
+import com.example.medical_center.dao.User;
+import com.example.medical_center.repositories.UserRepository;
+import com.example.medical_center.static_data.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class MedicalConfig implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.findByUsername("admin").isEmpty()) {
+            User user = User.builder()
+                    .role(Role.ROLE_ADMIN)
+                    .username("admin")
+                    .build();
+            userRepository.save(user);
+            System.out.println("U kry");
+        }
+    }
+}
