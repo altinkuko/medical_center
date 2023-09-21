359c0afdedf96c694372be847190b9eb8f200c91
altinkuko <altinkuko@gmail.com>
2023-09-21 17:11:32 +0200

    Finished services

--- src/main/java/com/example/medical_center/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/medical_center/services/impl/UserServiceImpl.java
@@ -1,41 +1,57 @@
 package com.example.medical_center.services.impl;
 
 import com.example.medical_center.dao.User;
+import com.example.medical_center.exceptions.GenericExceptions;
 import com.example.medical_center.repositories.UserRepository;
 import com.example.medical_center.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
     public User create(User entity) {
-        return null;
+        if (entity.getUserId() != null){
+            throw GenericExceptions.idNotNull();
+        } else {
+            userRepository.save(entity);
+            return entity;
+        }
     }
 
     @Override
     public User update(User entity) {
-        return null;
+        if (entity.getUserId() == null) {
+            throw GenericExceptions.idIsNull();
+        } else {
+            userRepository.save(entity);
+            return entity;
+        }
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(()-> GenericExceptions.notFound(id));
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public String delete(Long id) {
-        return null;
+        userRepository.deleteById(id);
+        return String.format("Record with id %d deleted", id);
     }
 }
